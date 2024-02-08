a261e6feeeb6c8b2ddee71454329fd49727c8fa0
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-02-08 14:40:54 +0100

    Added login logic through REST API
    When login form is submitted, handle the request through the API

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,16 +3,44 @@ import { useState } from "react"
 const Auth = () => {
 
     const [signup, setSignup] = useState(false)
-    const [credentials, setCredentials] = useState([])
+
+    const handleLogin = async(e) => {
+    // TODO: Add loading state
+        e.preventDefault();
+        const form = e.target;
+        const payload = {
+            username: form.username.value,
+            password: form.password.value, 
+        }
+    try {
+        const response = await fetch("https://faithhub-backend.fly.dev/auth/login", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        })
+        const result = await response.json()
+        // Login successful
+        if (response.status === 200){
+            const token = result.token;
+            localStorage.setItem("token", token);
+        // TODO: Handle is loggedin state
+        }
+    } catch(err) {
+        // TODO:  error and show message
+        console.log(err)
+    }
+    }
 
 // Login
     if (!signup) return (
     <div className=" bg-gray-100 w-screen h-screen fixed">
         <div className=" ml-auto mr-auto mt-52 text-center bg-white w-1/3
-         rounded-lg drop-shadow-md">
+         rounded-lg drop-shadow-md p-3">
             <h1 className=" pt-4 mb-6 font-Rubik text-2xl font-bold">
                 Welcome to FaithHub!</h1>
-            <form className="flex flex-col gap-6 mb-4">
+            <form className="flex flex-col gap-6 mb-4" onSubmit={handleLogin}>
                 <input className="ml-6 mr-6 p-3 border-gray-200 border-2 rounded-lg" 
                 type="text" name="username" placeholder="Username"></input>
                 <input className="ml-6 mr-6 p-3 border-gray-200 border-2 rounded-lg" 
@@ -21,7 +49,7 @@ const Auth = () => {
                    font-bold"
                  type="submit" value="Login"></input>
             </form>
-            <p className="pb-4 text-cyan-500">Don't have an account yet? Sign up here</p>
+            <p className="pb-4 text-cyan-500 underline">Don't have an account yet? Sign up here</p>
         </div>
     </div>
 
