c7533bfc1ef877768fba32b51f5f8a1a1d3dee50
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-02-10 17:38:56 +0100

    Added styling & icons/profile picture to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import { useSelector, useDispatch } from "react-redux"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMessage, faBell } from "@fortawesome/free-solid-svg-icons";
+
 
 const Header = () => {
 
@@ -17,15 +20,20 @@ const Header = () => {
 
     return (
         <div className="bg-white font-Rubik h-[3.5rem] drop-shadow 
-    fixed top-0 w-screen z-50 flex flex-row ">
+    fixed top-0 w-screen z-50 flex flex-row items-center ">
         <h1 className=" text-cyan-400 font-extrabold tracking-wide
-         text-2xl lg:text-3xl ml-8 mt-3 lg:mt-[0.6rem]">
+         text-2xl lg:text-3xl ml-8">
             FaithHub</h1>
         <form>
             <input type="text" placeholder="Search... " className="
-            bg-gray-100 mt-3 p-1 pl-3 pr-10 ml-8 rounded-large"></input>
+            bg-gray-100 p-1 pl-3 pr-10 ml-8 rounded-large"></input>
         </form>
-        <img src="https://faithhub-backend.fly.dev/uploads/uploads/de152381fff93c8bc37eac9d2b80e329"></img>
+        <div className="flex-grow"></div>
+        <FontAwesomeIcon icon={faMessage} className=" mr-6 w-6 h-6 text-cyan-400"/>
+        <FontAwesomeIcon icon={faBell} className=" mr-6 w-6 h-6   text-cyan-400"/>
+        {user.profile_picture &&  <img className=" mr-6 w-11 h-11 rounded-full object-cover"
+        src={`https://faithhub-backend.fly.dev/${user.profile_picture}`}/>}
+
     </div>
     )
 }
