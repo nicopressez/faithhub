3f5ecd99910cd929de84d3715022a5db7f7adbb6
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-02-16 11:01:34 +0100

    Added side navbar mobile styling
    The sizing of the navbars was adapted for mobile.
    For now the navbars are sitting on the page, toggle button to be added to header

--- src/components/Main/Nav.jsx
+++ src/components/Main/Nav.jsx
@@ -10,9 +10,10 @@ import { faCalendarDays,
 const Nav = () => {
 
     return (
-        <div className="bg-white h-[91%] w-[15%] fixed font-Rubik,
-        ml-5 mr-2 mt-[4.3rem] shadow-xl rounded-lg pt-5
-        flex flex-col gap-2 text-lg">
+        <div className="h-[38rem] p-2 mt-[3.5rem] gap-1
+        bg-white md:h-[91%] md:w-[15%] fixed font-Rubik,
+        md:ml-5 md:mr-2 md:mt-[4.3rem] shadow-xl rounded-lg md:pt-5
+        flex flex-col md:gap-2 md:text-lg z-10">
             <NavLink 
             to={"/home"}
             style={({ isActive }) => {
@@ -20,7 +21,7 @@ const Nav = () => {
                   backgroundColor: isActive ? "#F3F4F6" : "",
                 };
               }}
-              className={" pl-8 p-2 hover:bg-[#FAFAFA]"}
+              className={"pr-4 pl-4 md:pr-0 md:pl-8 p-2 hover:bg-[#FAFAFA]"}
             >
                 <FontAwesomeIcon
                     icon={faHouse}
@@ -34,7 +35,7 @@ const Nav = () => {
                   backgroundColor: isActive ? "" : "",
                 };
               }}
-              className={" pl-8 p-2 hover:bg-[#FAFAFA]"}>
+              className={"pr-4 pl-4 md:pr-0 md:pl-8 p-2 hover:bg-[#FAFAFA]"}>
                 <FontAwesomeIcon
                     icon={faChurch}
                     className="w-4 h-4 pr-2 text-gray-600"
@@ -47,7 +48,7 @@ const Nav = () => {
                   backgroundColor: isActive ? "" : "",
                 };
               }}
-              className={" pl-8 p-2 hover:bg-[#FAFAFA]"}>
+              className={"pr-4 pl-4 md:pr-0 md:pl-8 p-2 hover:bg-[#FAFAFA]"}>
                 <FontAwesomeIcon
                     icon={faUsers}
                     className="w-4 h-4 pr-2 text-gray-600"
@@ -60,7 +61,7 @@ const Nav = () => {
                   backgroundColor: isActive ? "" : "",
                 };
               }}
-              className={" pl-8 p-2 hover:bg-[#FAFAFA]"}>
+              className={" pr-4 pl-4 md:pr-0 md:pl-8 p-2 hover:bg-[#FAFAFA]"}>
                 <FontAwesomeIcon
                     icon={faCalendarDays}
                     className="w-4 h-4 pr-2 text-gray-600"
--- src/components/ProfileSettings/SettingsNav.jsx
+++ src/components/ProfileSettings/SettingsNav.jsx
@@ -23,9 +23,10 @@ const SettingsNav = () => {
   };
 
     if (user) return (
-        <div className="bg-white h-[91%] w-[15%] fixed font-Rubik,
-        ml-5 mr-2 mt-[4.3rem] shadow-xl rounded-lg pt-5
-        flex flex-col gap-2 text-lg">
+        <div className="h-[38rem] p-2 mt-[3.5rem] gap-1
+        bg-white md:h-[91%] md:w-[15%] fixed font-Rubik
+        md:ml-5 md:mr-2 md:mt-[4.3rem] shadow-xl md:rounded-lg md:pt-5
+        flex flex-col md:gap-2 md:text-lg z-10 md:p-0">
             <NavLink 
             to={`/profile/${user._id}/settings/info`}
             style={({ isActive }) => {
@@ -33,7 +34,7 @@ const SettingsNav = () => {
                   backgroundColor: isActive ? "#F3F4F6" : "",
                 };
               }}
-              className={" pl-8 p-2 hover:bg-[#FAFAFA]"}
+              className={"pr-4 pl-4 md:pr-0 md:pl-8 p-2 hover:bg-[#FAFAFA]"}
             >
                 <FontAwesomeIcon
                     icon={faUser}
@@ -48,7 +49,7 @@ const SettingsNav = () => {
                   backgroundColor: isActive ? "#F3F4F6" : "",
                 };
               }}
-              className={" pl-8 p-2 hover:bg-[#FAFAFA]"}>
+              className={"pr-4 pl-4 md:pr-0 md:pl-8 p-2 hover:bg-[#FAFAFA]"}>
                 <FontAwesomeIcon
                     icon={faGear}
                     className="w-4 h-4 pr-2 text-gray-600"
@@ -57,13 +58,14 @@ const SettingsNav = () => {
             </NavLink>
             <div className="flex-grow"></div>
             <button
-                className=" text-red-600 text-left mb-5
+                className=" text-red-600 text-left mb-2 md:mb-5
+                pr-4 pl-4 md:pr-0
                   hover:bg-[#FAFAFA] p-2"
                 onClick={handleLogout}
                 >
                 <FontAwesomeIcon
                     icon={faRightFromBracket}
-                    className="w-4 h-4 pr-2 pl-8"
+                    className="w-4 h-4 pr-2 md:pl-8"
                 />
                 Log out
             </button>
