dedaac49b4e176c7a2314dc516b4225907374dbd
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-03-11 16:39:23 +0100

    Added "loading" content on profile info

--- src/components/ProfilePage/ProfileLoading.jsx
+++ src/components/ProfilePage/ProfileLoading.jsx
@@ -8,6 +8,18 @@ const ProfileLoading = () => {
      rounded-lg drop-shadow-md p-1 pb-2 md:p-1 md:pb-2 font-Rubik 
      -mb-8 animate-pulse h-60"
     >
+      <div
+              className="bg-gradient-to-b from-gray-300 to-gray-300
+               border-4 border-cyan-400 p-[0.20rem] mt-3 mb-3 ml-auto mr-auto w-24 h-24 md:w-36 md:h-36 rounded-full
+             "
+
+            />
+            <div className="ml-auto mr-auto mt-4 bg-gradient-to-b from-gray-300 to-gray-400
+             w-64 h-3 rounded-lg "> </div>
+             <div className="ml-auto mr-auto mt-4 bg-gradient-to-b from-gray-300 to-gray-400
+             w-32 h-2 rounded-lg "> </div>
+             <div className="ml-auto mr-auto mt-4 bg-gradient-to-b from-gray-300 to-gray-400
+             w-40 h-2 rounded-lg "> </div>
         </div>
         </div>
         </>
